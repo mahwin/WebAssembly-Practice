698e410c53540a46ae27ac510a673e9e93cda4d4
youseock <mahwin7085@gmail.com>
2024-02-29 22:35:48 +0900

    chore: vite-wasm 관련된 플러그인

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { comlink } from "vite-plugin-comlink";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [react(), comlink()],
+  plugins: [react(), comlink(), wasm(), topLevelAwait()],
   worker: {
     plugins: () => [comlink()],
+  },
+  resolve: {
+    alias: {
+      "@wasm": "/src/wasm",
+    },
   },
 });
